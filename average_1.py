baf559af4bb6a11bc8d3c847aa1cbdafc93c62df
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2020-12-31 18:19:08 +0600

    Solved the problem 'Average 1'...

---
+++ average_1.py
@@ -0,0 +1,7 @@
+# Reading the grades of two student...
+first_student = float(input())
+second_student = float(input())
+# Calculating Median...
+MEDIA = ((first_student * 3.5) + (second_student * 7.5)) / (3.5+7.5)
+
+print("MEDIA = %0.5f" % MEDIA)
