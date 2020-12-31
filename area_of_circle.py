aa03c34b09556a4de832e85a414086fd88804ce1
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2020-12-31 15:56:27 +0600

    Solved the Problem 'Area of Circle'...

---
+++ area_of_circle.py
@@ -0,0 +1,11 @@
+# The formula of solution is (A = π . R2)....
+
+# Constant Value of PI..
+PI = 3.14159
+
+R = float(input())
+
+# Calculating Area of Circle...
+A = format(PI * (R ** 2), ".4f")
+
+print('A={}'.format(A))
