595e2be2e8c90a5902800e77039147a5c1c4c311
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2020-12-31 18:36:18 +0600

    Solved the problem 'Difference'...

---
+++ difference.py
@@ -0,0 +1,11 @@
+# Reading the numbers...
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+# Calculating the difference...
+difference = (A * B) - (C * D)
+
+# Printing the result...
+print('DIFERENCA = %d' % difference)
