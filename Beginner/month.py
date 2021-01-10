40ab4734edbc8f5ffbdc626fd65c2e5c7fb70c13
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-10 23:05:16 +0600

    Solved the Problem 'Month'...

---
+++ Beginner/month.py
@@ -0,0 +1,7 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+
+month = int(input())
+
+# Printing the Results...
+print(months[month - 1])
