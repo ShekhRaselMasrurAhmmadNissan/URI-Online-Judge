643f1cc18f9180d9bfdb8af5aaa5cae2e5279c2f
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-10 21:35:16 +0600

    Solved the Problem 'DDD'...

---
+++ Beginner/ddd.py
@@ -0,0 +1,22 @@
+# Reading the Data...
+serial_number = int(input())
+
+# Checking the conditions...
+if (serial_number == 61):
+    print('Brasilia')
+elif (serial_number == 71):
+    print('Salvador')
+elif (serial_number == 11):
+    print('Sao Paulo')
+elif (serial_number == 21):
+    print('Rio de Janeiro')
+elif (serial_number == 32):
+    print('Juiz de Fora')
+elif (serial_number == 19):
+    print('Campinas')
+elif (serial_number == 27):
+    print('Vitoria')
+elif (serial_number == 31):
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
