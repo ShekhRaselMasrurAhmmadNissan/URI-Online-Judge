51550fdc6f720e586c50d48c6a541c6b7f202038
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-18 16:30:41 +0600

    Solved the Problem ' Sequence numbers and sum'...

---
+++ Beginner/1101_sequence_numbers_and_sum.py
@@ -0,0 +1,14 @@
+#URI Online Judge | 1101 Sequência de Números e Soma
+while(True):
+  Lista = []
+  a, b = map(int, input().split())
+  if(a <= 0) or (b <= 0):
+    break
+  else:
+    if(a > b):
+      a, b = b, a
+    for i in range(a, b + 1):
+      Lista.append(i)
+    Soma = 'Sum=%d' %sum(Lista)
+    Lista.append(Soma)
+    print(' '.join(map(str, Lista)))
