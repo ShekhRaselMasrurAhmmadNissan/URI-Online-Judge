828f64672557148147fabd821b71d6137c450805
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-18 15:16:36 +0600

    Solved the Problem 'Sum of Consecutive Number 2'...

--- Beginner/1099_sum_of_consecutive_odd_numbers_2.py
+++ Beginner/1099_sum_of_consecutive_odd_numbers_2.py
@@ -1,25 +1,23 @@
 # Reading the Data...
 n = int(input())
 results = list()
-while True:
-    if 
-        for i in range(n):
-            X, Y = map(int, input().split())
+for i in range(n):
+    X, Y = map(int, input().split())
 
-            if (X > Y):
-                start_point = Y
-                end_point = X
-            else:
-                start_point = X
-                end_point = Y
+    if (X > Y):
+        start_point = Y
+        end_point = X
+    else:
+        start_point = X
+        end_point = Y
 
-            sum = 0
+    sum = 0
 
-            for number in range(start_point + 1, end_point):
-                if (number % 2 != 0):
-                    sum += number
+    for number in range(start_point + 1, end_point):
+        if (number % 2 != 0):
+            sum += number
 
-            results.append(sum)
+    results.append(sum)
 
-        for result in results:
-            print(result)
+for result in results:
+    print(result)
