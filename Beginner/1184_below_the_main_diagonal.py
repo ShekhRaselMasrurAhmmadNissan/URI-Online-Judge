950a571287b034131be591d8f508026ee9bf238f
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-01-21 17:52:57 +0600

    Solved the Problem 'Below The Main Diagonal'...

---
+++ Beginner/1184_below_the_main_diagonal.py
@@ -0,0 +1,17 @@
+operation = input()
+sum = 0
+count = 0
+
+for i in range(12):
+    for j in range(12):
+        value = float(input())
+
+        if (i > j):
+            sum += value
+            count += 1
+
+if (operation == 'S'):
+    print(f'{sum:0.1f}')
+elif(operation == 'M'):
+    average = sum / count
+    print(f'{average:0.1f}')
